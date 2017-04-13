e455123c4904917eb282cd6237bd310e256e1225
Sergey Bondar <SBT.SEBondar@sberbank.ru>
2017-04-13 17:52:09 +0300

    chore(*) build because no published

---
+++ dist/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var chalk = require('chalk');
+var fs = require('fs');
+var install = require('spawn-npm-install');
+var Package = require('./package');
+
+var die = function die(message) {
+    return console.error(chalk.bold.red(message));
+};
+var warn = function warn(message) {
+    return console.warn(chalk.yellow(message));
+};
+var log = function warn(message) {
+    return console.log(chalk.green(message));
+};
+
+fs.readFile('package.json', 'utf-8', function (error, contents) {
+
+    if (contents === undefined) {
+        return die('There doesn\'t seem to be a package.json here');
+    }
+
+    var packageContents = new Package(contents);
+
+    if (!packageContents.isValid()) {
+        return die('Invalid package.json contents');
+    }
+
+    if (!packageContents.hasPeerDependencies()) {
+        return warn('This package doesn\'t seem to have any peerDependencies');
+    }
+
+    var peerDependencies = packageContents.peerDependencies;
+
+    var peerInstallOptions = packageContents.peerInstallOptions;
+
+    log('Install ' + Object.keys(peerDependencies).length + ' peerDependencies...');
+    var packages = Object.keys(peerDependencies).map(function (key) {
+        var name = key + '@' + peerDependencies[key];
+        return name;
+    });
+
+    var installDependencies = function installDependencies() {
+        var name = packages.shift();
+        install(name, peerInstallOptions, function () {
+            log(name);
+            installDependencies();
+        });
+    };
+    installDependencies();
+});
